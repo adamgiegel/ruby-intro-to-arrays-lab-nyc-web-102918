3cf93721e0b5b2da8843406a1aaddbc7037ab2a8
Adam Giegel <adam.giegel@gmail.com>
2018-10-16 16:11:31 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -3,7 +3,7 @@ def instantiate_new_array
   @my_new_array = instantiate_new_array
 end
 
-def instantiate_new_array
+def array_with_two_elements
   instantiate_new_array = ["apple", "orange"]
   @my_new_array = instantiate_new_array
 end
