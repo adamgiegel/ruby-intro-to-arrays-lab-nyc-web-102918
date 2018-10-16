2c0d642ab1bdb3b9c3602b4aa676614eca054281
Adam Giegel <adam.giegel@gmail.com>
2018-10-16 16:14:55 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -7,3 +7,8 @@ def array_with_two_elements
   array_with_two_elements = ["apple", "orange"]
   @my_new_array = array_with_two_elements
 end
+
+def first_element(my_first_element)
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  my_first_element = @taylor_swift.first
+end
